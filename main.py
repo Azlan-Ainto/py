59179ade439cd8783bfab4febfefe4052a3d93f8
Azlan Ainto <azlanainto97@gmail.com>
2026-05-24 12:32:01 +0200

    Variablen, Zuweisungen und mathematische Operatoren. Ausgabe mit print("") und print(f,"")

--- main.py
+++ main.py
@@ -9,23 +9,18 @@ difference = a - b
 square_root = a ** (1 / b)
 cubic_root = a ** (1 / 3)
 
-
 print("Die Aufgabe:",a ,"+", b)
 print("Das Ergebnis:", sum)
-
 
 # String-Literale
 print(f"Die Aufgabe: {a} + {b} ")
 print(f"Das Ergebnis: {sum}")
-
 print(f"Produkt: {a} * {b} = {product}")
 print(f"Difference: {a} - {b} = {difference}")
 
 #  Zuweisung der Variablen mit neuen Werten
 a = 8
 b = 4
-power = a ** b
-
 power = a ** b
 print(f"Power: {a}^{b} = {power}")
 print(f"Square_root: {a}^{b} = {square_root}")
