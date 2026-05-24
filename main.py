9a6ea2d7d25bfb65e5b2824c8c535c7c592b6075
Azlan Ainto <azlanainto97@gmail.com>
2026-05-24 14:04:16 +0200

    Eingabe einer beliebigen Zeichenkette und einer Zahl. Konvertieren von der string zu int.

---
+++ main.py
@@ -0,0 +1,9 @@
+
+# Eingabe einer Zeichenkette
+
+nachricht = input("Geben Sie Ihre Nachricht ein: ")
+print("Ihre Nachricht :",nachricht)
+
+print("Wie alt sind Sie? ")
+alter = int(input())
+print(f"Du bist {alter} jahre alt.")
